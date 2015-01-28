ebfb3db339fd18c545d6deeda1a99ca9dda6d9d7
Tyler Brown & Tucker Mayo <student@epicodus.com>
2015-01-28 14:38:47 -0800

    Finalize all sinatra files and completes edit and delete functionality

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ post("/questions") do
 end
 
 get("/surveys/:id/edit") do
-  @survey = Survey.find(paramsfetch("id").to_i())
+  @survey = Survey.find(params.fetch("id").to_i())
   erb(:survey_edit)
 end
 
@@ -37,7 +37,7 @@ patch("/surveys/:id") do
   name = params.fetch("name")
   @survey = Survey.find(params.fetch("id").to_i())
   @survey.update({:name => name})
-  erb(:index)
+  erb(:survey)
 end
 
 delete("/surveys/:id") do
@@ -45,4 +45,24 @@ delete("/surveys/:id") do
   @survey.delete
   @surveys = Survey.all()
   erb(:index)
+end
+
+get("/questions/:id/edit") do
+  @question = Question.find(params.fetch("id").to_i())
+  erb(:question_edit)
+end
+
+patch("/questions/:id") do
+  description = params.fetch("description")
+  @question = Question.find(params.fetch("id").to_i())
+  @question.update({:description => description})
+  @survey = Survey.find(params.fetch("id").to_i())
+  erb(:survey)
+end
+
+delete("/questions/:id") do
+  @question = Question.find(params.fetch("id").to_i())
+  @question.delete()
+  @questions = Question.all()
+  erb(:survey)
 end
