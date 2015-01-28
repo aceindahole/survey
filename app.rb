2a7e56471e521621e42aa82ad3f1120466092d79
Tyler Brown & Tucker Mayo <student@epicodus.com>
2015-01-28 10:53:09 -0800

    add app.rb and index.erb file

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+post('/survey') do
+  name = params.fetch("name")
+  @survey = Survey.create({:name => name})
+  @surveys = Survey.all
+  erb(:index)
+end
